9766200432356050cc9749b0f9e4fe0384c59673
DominikPyrek <pyrekdominik2025@gmail.com>
2025-02-17 12:22:56 +0100

    Inital Commit First creation of Nav

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import Nav from './components/Nav/Nav.js'
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import home from './Home.svg'
+import github from './Github.svg'
+import mail from './Mail.svg'
+import './Nav.css'
+
+
+export default function Nav() {
+  return (
+    <div className="mainContainer">
+      <div className="firstContainer">
+        <img className="home" src={home} />
+        <img className="github" src={github} />
+      </div>
+      <div className="secondContainer">
+        <p>Reach out to me </p>
+        <img src={mail} />
+      </div>
+    </div>
+  )
+}
