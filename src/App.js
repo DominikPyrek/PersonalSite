5853ebd263d604f3b5dc8076707517010914df52
DominikPyrek <pyrekdominik2025@gmail.com>
2025-02-18 10:49:02 +0100

    Buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import Nav from './components/Nav/Nav.js'
 import Hero from './components/Hero/Hero.js'
+import Buttons from './components/Buttons/Buttons.js'
+
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Buttons />
     </div>
   );
 }
---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,11 @@
+import './Buttons.css'
+
+export default function Buttons() {
+  return (
+    <div className="buttons">
+      <button>Skills</button>
+      <button>Projects</button>
+      <button>About me</button>
+    </div>
+  )
+}
