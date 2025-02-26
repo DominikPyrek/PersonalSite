814387522d24a0e78ef102bfc34f1de289e687d0
DominikPyrek <pyrekdominik2025@gmail.com>
2025-02-26 15:35:09 +0100

    Animations and clean up

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,16 @@
 import icon from './icon.svg'
 import './Hero.css'
+import github from '../Nav/Github.svg'
 
 export default function Hero() {
   return (
-    <div className="hero">
-      <h1>Dominik Pyrek</h1>
-      <img src={icon} className="iconHero" />
-      <h2>Fullstack WebDeveloper</h2>
-    </div>
+    <>
+      <div className="hero">
+        <h1 className="heading">Dominik Pyrek</h1>
+        <div className="wheel"><img src={icon} className="iconHero" /><img src={github} className="wheelIcon" /><img src={github} className="wheelIcon" /><img src={github} className="wheelIcon" /></div>
+        <h2 className="heading2">Fullstack WebDeveloper</h2>
+      </div>
+    </>
   )
 
 }
